02de2221779a7d4fe7a3d3876f8bf04d4fbea354
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-11-19 00:10:57 -0300

    update

--- back/ecommerce-ezequielbonilla/src/app.module.ts
+++ back/ecommerce-ezequielbonilla/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users-old/users.module';
-import { AuthModule } from './auth-old/auth.module';
-import { ProductModule } from './products-old/product.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductModule, ProductsModule],
+  imports: [UsersModule, AuthModule, ProductsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
