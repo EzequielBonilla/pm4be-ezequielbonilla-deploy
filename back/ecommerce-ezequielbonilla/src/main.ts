edfd28d051bf9d41592f35aa63d68df19c6061cf
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-12-09 18:03:48 -0300

    create db and run migrations on compose

--- back/ecommerce-ezequielbonilla/src/main.ts
+++ back/ecommerce-ezequielbonilla/src/main.ts
@@ -6,6 +6,7 @@ import { ProductsSeed } from './seeds/products/products.seed';
 import { ValidationPipe } from '@nestjs/common';
 import { UsersSeed } from './seeds/users/users.seed';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,38 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
+
+  const dataSource = app.get(DataSource);
+  const databaseName = 'pm4be_ecommerce';
+  const queryRunner = dataSource.createQueryRunner();
+
+  try {
+    await queryRunner.connect();
+
+    const result = await queryRunner.query(`
+      SELECT 1 FROM pg_database WHERE datname = '${databaseName}'
+    `);
+
+    if (result.length === 0) {
+      console.log(`La base de datos ${databaseName} no existe. Creándola...`);
+      await queryRunner.query(`CREATE DATABASE ${databaseName}`);
+      console.log(`Base de datos ${databaseName} creada.`);
+    }
+
+    await dataSource.setOptions({
+      database: databaseName,
+    });
+
+    console.log(`Conectando a la base de datos ${databaseName}...`);
+
+    await dataSource.initialize();
+    await dataSource.runMigrations();
+    console.log('Migraciones ejecutadas con éxito');
+  } catch (error) {
+    console.error('Error al conectar o crear la base de datos', error);
+  } finally {
+    await queryRunner.release();
+  }
 
   const categoriesSeed = app.get(CategoriesSeed);
   await categoriesSeed.seed();
