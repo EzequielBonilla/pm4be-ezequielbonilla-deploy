5fb260479f974f1f5faa0a6edd0a799494a1b6a8
EzequielBonilla <ezequiel.bonilla92@gmail.com>
2024-12-10 17:32:15 -0300

    deploy ok

--- back/ecommerce-ezequielbonilla/src/app.service.ts
+++ back/ecommerce-ezequielbonilla/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Bienvenido a mi PM4 de BackEnd. ingresa a /docs para la documentación en Swagger';
   }
 }
